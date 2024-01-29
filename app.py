0d8a2303565ac5afe219f32fbb4333426c4d28bc
Gabriel Dagostim <105657571+gabrieldagostim@users.noreply.github.com>
2024-01-28 23:13:50 -0300

    alterado app.py
    vamos trabalhar com a bliblioteca streamlit, aprendendo ainda kk

--- app.py
+++ app.py
@@ -3,33 +3,58 @@ import pandas as pd
 import plotly.express as px
 
 # Carregue o DataFrame a partir do arquivo Excel
-df = pd.read_excel('Relatório_comercial_agrupado.xlsx')
+df = pd.read_excel('relatório_agrupado_guerrilha.xlsx')
+print(df.info())
 
 # Adicione elementos Streamlit conforme necessário
 st.title('Relatório Comercial - Visualização Interativa')
 
 # Adicione um seletor de loja para filtrar dados por loja
-selected_loja = st.selectbox('Selecione a Loja', df['loja'].unique())
-filtered_df = df[df['loja'] == selected_loja]
+lojas_disponiveis = ['Rede'] + list(df['loja'].unique())
+selected_loja = st.selectbox('Selecione a loja', lojas_disponiveis)
 
-# Adicione um gráfico de barras interativo para as vendas por loja
-fig = px.bar(filtered_df, x='exp_nome', y='venda_30', title=f'Vendas por Loja - {selected_loja}')
-st.plotly_chart(fig)
+# Se "Rede" for selecionado, exibe todos os dados sem filtrar por loja
+if selected_loja == 'Rede':
+    filtered_df = df
+else:
+    filtered_df = df[df['loja'] == selected_loja]
 
-# Adicione uma tabela interativa com os dados filtrados
-st.write(f'Dados para a Loja {selected_loja}:')
-st.dataframe(filtered_df)
+selected_comprador = st.selectbox('Selecione o comprador', filtered_df['NOME_COMPRADOR'].unique())
+filtered_df = filtered_df[filtered_df['NOME_COMPRADOR'] == selected_comprador]
+
+filtered_df['analisar'] = filtered_df['analisar'].replace('⚠️ Atenção Expositor Maior que a venda!', '⚠️').replace('✅ Ok', '✅')
+
+# Calcular a razão Expositor / V30 dias e ordenar a tabela por essa razão em ordem decrescente
+filtered_df['razao_expositor_v30'] = filtered_df['qtd_expositor_posicao'] / filtered_df['venda_v30']
+filtered_df = filtered_df.sort_values(by='razao_expositor_v30', ascending=False)
+
+# Adicione uma tabela interativa com os dados filtrados (excluindo a coluna 'razao_expositor_v30')
+filtered_df_display = filtered_df[['loja', 'codigo_sku', 'DESCRICAO', 'venda_v30', 'qtd_expositor_posicao', 'analisar']]
+filtered_df_display['codigo_sku'] = filtered_df_display['codigo_sku'].astype(str)
+colunas_renomear = {
+    'loja': 'Loja',
+    'codigo_sku': 'Código',
+    'DESCRICAO': 'Produto',
+    'venda_v30': 'V30 dias',
+    'qtd_expositor_posicao': 'Expositor',
+    'analisar': 'Análise',
+}
+filtered_df_display.rename(columns=colunas_renomear, inplace=True)
+
+if selected_loja == 'Rede':
+    st.write(f'Dados para a {selected_loja}:')
+else:
+    st.write(f'Dados para a loja {selected_loja}:')
+
+st.dataframe(filtered_df_display, hide_index=True, use_container_width=True)
 
 # Adicione estatísticas descritivas para as vendas
 st.write('Estatísticas Descritivas para Vendas:')
-st.write(f'Média de Vendas: {filtered_df["venda_30"].mean()}')
-st.write(f'Mediana de Vendas: {filtered_df["venda_30"].median()}')
-
-# Adicione um mapa interativo (supondo que você tenha colunas de Latitude e Longitude no DataFrame)
-if 'Latitude' in df.columns and 'Longitude' in df.columns:
-    st.map(df[['Latitude', 'Longitude']].dropna())
+st.write(f'Média de Vendas: {filtered_df["Venda 30 Dias"].mean()}')
+st.write(f'Mediana de Vendas: {filtered_df["Venda 30 Dias"].median()}')
 
 # Adicione um botão para exportar os dados filtrados para CSV
-if st.button('Exportar Dados para CSV'):
-    filtered_df.to_csv('dados_exportados.csv', index=False)
+if st.button('Exportar Dados para Excel'):
+    filtered_df.to_excel('dados_exportados.xlsx', index=False)
     st.success('Dados exportados com sucesso!')
+
